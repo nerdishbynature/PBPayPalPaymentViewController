7a93291e3faeb7a5958798e436eef9ed6c9352bf
Piet Brauer <piet@nerdishbynature.com>
2013-03-08 23:11:02 +0100

    Initial Commit

---
+++ PayPalWrapper/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  PayPalWrapper
+//
+//  Created by Piet Brauer on 08.03.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PayPalWrapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PayPalWrapper
+//
+//  Created by Piet Brauer on 08.03.13.
+//  Copyright (c) 2013 Piet Brauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PBAppDelegate class]));
+    }
+}
